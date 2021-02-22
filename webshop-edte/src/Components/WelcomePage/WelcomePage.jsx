c9d7cf3c31028f056ab723ad05f518edebb002d1
1337DS <77491801+1337DS@users.noreply.github.com>
2021-02-22 23:31:57 +0100

    worked on router

--- webshop-edte/src/Components/WelcomePage/WelcomePage.jsx
+++ webshop-edte/src/Components/WelcomePage/WelcomePage.jsx
@@ -1,45 +1,25 @@
 import React, { Component } from 'react';
+import data from '../../data';
 
-class WelcomePage extends Component {
-    render(){
-        return (
-            
-            <html>
-                
-                <head>
-                    <title>Rares für Bares | Webshop</title>
-                    <meta charset="utf-8" />
-                    
-                </head>
 
-                <body>
-                    <div>
-                        <header>
-                            Das ist der Header
-                        </header>
-                    
-                        <aside>
-                            Shopping Categories
-                        </aside>
-                        <main>
-                            <ul>
-                                <li>Product</li>
-                                <li>product 2</li>
-                                <li>Product 3</li>
-
-                                
-                            </ul>
-                        </main>
-                        <footer>All rights reserved 2021</footer>
-
-                    </div>
-                </body>
-
-            </html>
-
-            
-        )
-    }
+function HomeScreen(props) {
+    return <div>
+                <ul className="products">
+                    {
+                    data.products.map(product =>             
+                    <li>
+                        <div className="product">
+                        <img className="product-image" src={product.image} alt="product"></img>
+                        <div className="product-name">
+                            <a href="Jetpack.html">{product.name}</a>
+                        </div>
+                        <div className="product-description">{product.description}</div>
+                        <div className="product-price">{product.price}</div>
+                        </div>
+                    </li>)
+                    }
+                </ul>
+         
+            </div>
 }
-
-export default WelcomePage;
+export default HomeScreen;
