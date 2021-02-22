8fa1bf96b4f6c1b99a1d648c5f1774ab45081fa0
1337DS <77491801+1337DS@users.noreply.github.com>
2021-02-22 22:35:35 +0100

    created data.js with item specs

---
+++ webshop-edte/src/data.js
@@ -0,0 +1,24 @@
+export default {
+    products:[
+        {
+        name: 'Secuan Sauce',
+        description: 'Sehr Lecker!',
+        image: '/images/sauce.jpeg',
+        price: '100$',
+        },
+
+        {
+        name: 'Kein Flammenwerfer',
+        description: 'Sehr effektives Gerät zum Grillen von Burger',
+        image: '/images/NAF.jpg',
+        price: '500$',
+        },
+
+        {
+        name: 'Secuan Sauce',
+        description: 'Sehr Lecker!',
+        image: '/images/sauce.jpeg',
+        price: '100$',
+        },
+]
+}
