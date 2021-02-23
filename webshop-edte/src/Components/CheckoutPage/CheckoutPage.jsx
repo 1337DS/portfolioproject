21a0877b0a6a62ad996cc9af12c7293a22e6b544
1337DS <77491801+1337DS@users.noreply.github.com>
2021-02-23 01:24:33 +0100

    comment for commit

---
+++ webshop-edte/src/Components/CheckoutPage/CheckoutPage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ShoppingCart extends Component {
+    render() {
+        return (
+            <div>
+                 This is Checkout Page
+                
+            </div>
+        );
+    }
+}
+
+export default ShoppingCart;
